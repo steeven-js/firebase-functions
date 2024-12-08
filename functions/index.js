57a0d20a8e1c35105890a98b1645fd4176e88295
steeven-js <jacques.steeven@gmail.com>
2024-12-08 18:54:38 -0400

    Ajout de la dépendance firebase-admin, création de nouvelles fonctions pour ajouter des messages à Firestore et convertir en majuscules, et ajout d'un fichier .gcloudignore.

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,42 @@
-import * as v2 from 'firebase-functions/v2';
+// Le SDK Cloud Functions pour Firebase pour créer des fonctions Cloud et des déclencheurs.
+import { logger } from "firebase-functions";
+import { onRequest } from "firebase-functions/v2/https";
+import { onDocumentCreated } from "firebase-functions/v2/firestore";
 
-export const helloWorld = v2.https.onRequest((request, response) => {
-    // Extract the parameter from the URL path
-    const name = request.path.split('/').pop();
+// Le SDK Firebase Admin pour accéder à Firestore.
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
-    // Define the items object
-    const items = {
-        lamp: 'This is a lamp',
-        table: 'This is a table',
-    };
+initializeApp();
 
-    // Fetch the corresponding message or return a default message
-    const message = items[name || ''] || 'Item not found';
+// Récupère le paramètre "text" passé à ce point de terminaison HTTP 
+// et l'insère dans Firestore sous le chemin /messages/:documentId/original
+export const addmessage = onRequest(async (req, res) => {
+    // Récupère le paramètre "text".
+    const original = req.query.text;
+    // Ajoute le nouveau message dans Firestore en utilisant le SDK Firebase Admin.
+    const writeResult = await getFirestore()
+    .collection("messages")
+    .add({ original: original });
+    // Renvoie un message confirmant que le message a été ajouté avec succès.
+    res.json({ result: `Message with ID: ${writeResult.id} added.` });
+});
 
-    // Send the response as HTML
-    response.send(`<h1>${message}</h1>`);
+// Écoute les nouveaux messages ajoutés à /messages/:documentId/original
+// et enregistre une version en majuscules du message 
+// dans /messages/:documentId/uppercase
+export const makeuppercase = onDocumentCreated("/messages/{documentId}", (event) => {
+    // Récupère la valeur actuelle écrite dans Firestore.
+    const original = event.data.data().original;
+
+    // Accède au paramètre {documentId} avec event.params
+    logger.log("Conversion en majuscules", event.params.documentId, original);
+
+    const uppercase = original.toUpperCase();
+
+    // Vous devez retourner une Promesse lorsque vous effectuez
+    // des tâches asynchrones dans une fonction,
+    // comme écrire dans Firestore.
+    // La définition d'un champ 'uppercase' dans le document Firestore retourne une Promesse.
+    return event.data.ref.set({ uppercase }, { merge: true });
 });
